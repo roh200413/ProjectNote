4c0fdafafed37347ca722c49649d9d67fcffca70
노승희 <39451858+roh200413@users.noreply.github.com>
2026-03-11 21:57:51 +0900

    Add missing project detail API for print page data load

--- server/domains/data_updates/api.py
+++ server/domains/data_updates/api.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_GET, require_http_methods
 
-from server.application.web_support import data_update_repository, login_required_page, page_context
+from server.application.web_support import data_update_repository, login_required_page
 
 
 @require_http_methods(["GET", "POST"])
@@ -18,8 +18,4 @@ def data_updates_api(request):
 @ensure_csrf_cookie
 @login_required_page
 def data_updates_page(request):
-    return render(
-        request,
-        "workflow/final_download.html",
-        page_context(request, {"report_name": "data-updates"}),
-    )
+    return redirect("/data-updates")
